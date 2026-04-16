088350c1cd79d61d0d4f2e16e964fdee562b54c4
LGasecki <gaseckiyt@gmail.com>
2026-04-17 01:08:14 +0200

    Unplugged unnecesery cables

--- main/ILI_9341_files/hardware.h
+++ main/ILI_9341_files/hardware.h
@@ -4,22 +4,26 @@
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
 
-// --- Piny SPI (Dostosuj do swoich połączeń) ---
-#define PIN_NUM_MISO  8
-#define PIN_NUM_MOSI  10
-#define PIN_NUM_CLK   9
+// --- SPI2: Magistrala dla EKRANU ---
+#define PIN_NUM_TFT_MOSI 10
+#define PIN_NUM_TFT_CLK  9
+#define PIN_NUM_TFT_MISO -1  // Wyłączamy MISO ekranu, aby zwolnić pin 8
 
-// --- Piny Ekranu TFT ---
-#define PIN_NUM_TFT_CS 13
-#define PIN_NUM_TFT_DC 11
+// --- SPI3: Magistrala dla DOTYKU (Twoje nowe piny) ---
+#define PIN_NUM_TOUCH_MISO 16 // T_DO
+#define PIN_NUM_TOUCH_MOSI 17 // T_DIN
+#define PIN_NUM_TOUCH_CLK  8  // T_CLK
+
+// --- Piny sterujące Ekranem ---
+#define PIN_NUM_TFT_CS  13
+#define PIN_NUM_TFT_DC  11
 #define PIN_NUM_TFT_RST 12
 #define PIN_NUM_TFT_LED 46
 
-// --- Piny Dotyku (XPT2046) ---
+// --- Piny sterujące Dotykiem ---
 #define PIN_NUM_TOUCH_CS  18
 #define PIN_NUM_TOUCH_IRQ 15
 
-// Globalne uchwyty (handle) do urządzeń SPI
 extern spi_device_handle_t spi_tft;
 extern spi_device_handle_t spi_touch;
 
--- main/main.c
+++ main/main.c
@@ -12,45 +12,51 @@ spi_device_handle_t spi_tft;
 spi_device_handle_t spi_touch;
 
 void init_hardware() {
-    // 1. Konfiguracja magistrali SPI
-    spi_bus_config_t buscfg = {
-        .miso_io_num = PIN_NUM_MISO,
-        .mosi_io_num = PIN_NUM_MOSI,
-        .sclk_io_num = PIN_NUM_CLK,
+    // 1. Inicjalizacja SPI2 dla Wyświetlacza
+    spi_bus_config_t tft_buscfg = {
+        .miso_io_num = PIN_NUM_TFT_MISO,
+        .mosi_io_num = PIN_NUM_TFT_MOSI,
+        .sclk_io_num = PIN_NUM_TFT_CLK,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = TFT_WIDTH * TFT_HEIGHT * 2 + 8 // Duży bufor dla szybkiego rysowania
+        .max_transfer_sz = TFT_WIDTH * TFT_HEIGHT * 2 + 8
     };
-    // Inicjalizujemy SPI2_HOST
-    spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    spi_bus_initialize(SPI2_HOST, &tft_buscfg, SPI_DMA_CH_AUTO);
 
-    // 2. Konfiguracja urządzenia TFT (Ekran) - Szybko (np. 40 MHz)
     spi_device_interface_config_t tft_cfg = {
         .clock_speed_hz = 40 * 1000 * 1000, 
         .mode = 0,                               
-        .spics_io_num = PIN_NUM_TFT_CS,     // System sam steruje CS!
+        .spics_io_num = PIN_NUM_TFT_CS,     
         .queue_size = 7,                          
     };
     spi_bus_add_device(SPI2_HOST, &tft_cfg, &spi_tft);
 
-    // 3. Konfiguracja urządzenia Touch (Dotyk) - Wolno (np. 2 MHz)
+    // 2. Inicjalizacja SPI3 dla Dotyku
+    spi_bus_config_t touch_buscfg = {
+        .miso_io_num = PIN_NUM_TOUCH_MISO,
+        .mosi_io_num = PIN_NUM_TOUCH_MOSI,
+        .sclk_io_num = PIN_NUM_TOUCH_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1
+    };
+    spi_bus_initialize(SPI3_HOST, &touch_buscfg, SPI_DMA_CH_AUTO);
+
     spi_device_interface_config_t touch_cfg = {
-        .clock_speed_hz = 2 * 1000 * 1000,  // 2 MHz wystarczy i jest stabilne
+        .clock_speed_hz = 2 * 1000 * 1000, 
         .mode = 0,                               
-        .spics_io_num = PIN_NUM_TOUCH_CS,   // System sam steruje CS!
+        .spics_io_num = PIN_NUM_TOUCH_CS,   
         .queue_size = 3,                          
     };
-    spi_bus_add_device(SPI2_HOST, &touch_cfg, &spi_touch);
+    spi_bus_add_device(SPI3_HOST, &touch_cfg, &spi_touch);
 
-// 4. Konfiguracja pozostałych pinów (DC, RST, IRQ, LED)
+    // 3. Konfiguracja pinów sterujących (DC, RST, IRQ, LED)
     gpio_set_direction(PIN_NUM_TFT_DC, GPIO_MODE_OUTPUT);
     gpio_set_direction(PIN_NUM_TFT_RST, GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_NUM_TFT_LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(PIN_NUM_TFT_LED, 1); // Włącz podświetlenie
+    
     gpio_set_direction(PIN_NUM_TOUCH_IRQ, GPIO_MODE_INPUT);
     gpio_set_pull_mode(PIN_NUM_TOUCH_IRQ, GPIO_PULLUP_ONLY);
-
-// 5. Konfiguracja podświetlenia LED ---
-    gpio_set_direction(PIN_NUM_TFT_LED, GPIO_MODE_OUTPUT);
-    gpio_set_level(PIN_NUM_TFT_LED, 1); // 1 = Włącz podświetlenie na 100%, 0 = Wyłącz
 }
 
 void app_main(void) {
